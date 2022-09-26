2c940105f0b0fd19669a6460d13b5a4c17d2d4f5
SalikhovD <salikhvdima@gmail.com>
2022-09-26 23:44:49 +0300

    Исправление замечаний

--- src/main/java/com/salikhov/crud/controller/UserController.java
+++ src/main/java/com/salikhov/crud/controller/UserController.java
@@ -6,10 +6,7 @@ import com.salikhov.crud.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,27 +27,27 @@ public class UserController {
         return "all-users";
     }
 
-    @RequestMapping(value = "/delete-user")
+    @PostMapping(value = "/delete-user")
     public String deleteUser(@RequestParam Long id) {
         service.deleteUser(id);
         return "redirect:/";
     }
 
-    @RequestMapping("/updateInfo")
+    @PostMapping("/updateInfo")
     public String updateUser(@RequestParam Long id, Model model) {
         User user = service.getUser(id);
         model.addAttribute("user", user);
         return "user-info";
     }
 
-    @RequestMapping("/new-user")
+    @GetMapping("/new-user")
     public String saveUser(Model model) {
         User user = new User();
         model.addAttribute("user", user);
         return "user-info";
     }
 
-    @RequestMapping("/save-or-update-user")
+    @PostMapping("/save-or-update-user")
     public String userInfo(@ModelAttribute("user") User user) {
         service.saveOrUpdate(user);
         return "redirect:/";
--- src/main/java/com/salikhov/crud/dao/UserDaoImp.java
+++ src/main/java/com/salikhov/crud/dao/UserDaoImp.java
@@ -1,12 +1,11 @@
 package com.salikhov.crud.dao;
 
 import com.salikhov.crud.entity.User;
-import org.hibernate.Session;
-import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
@@ -16,32 +15,28 @@ public class UserDaoImp implements UserDao {
 
     @Override
     public void add(User user) {
-        Session session = entityManager.unwrap(Session.class);
-        session.save(user);
+        entityManager.persist(user);
     }
 
     @Override
     public void deleteUser(Long id) {
-        Session session = entityManager.unwrap(Session.class);
-        session.remove(session.get(User.class, id));
+        User user = entityManager.find(User.class, id);
+        entityManager.remove(user);
     }
 
     @Override
     public void saveOrUpdate(User user) {
-        Session session = entityManager.unwrap(Session.class);
-        session.saveOrUpdate(user);
+        entityManager.merge(user);
     }
 
     @Override
     public List<User> listUsers() {
-        Session session = entityManager.unwrap(Session.class);
-        Query<User> query = session.createQuery("from User", User.class);
+        TypedQuery<User> query = entityManager.createQuery("from User", User.class);
         return query.getResultList();
     }
 
     @Override
     public User getUser(Long id) {
-        Session session = entityManager.unwrap(Session.class);
-        return session.get(User.class, id);
+        return entityManager.find(User.class, id);
     }
 }
